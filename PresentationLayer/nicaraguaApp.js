9458ba1b05cf90b45c8aa6165465ca0d6b342e26
Giulliano Siviero <gsilvazanottisiviero@scu.edu>
2022-12-11 19:45:07 -0800

    Documentation and Removal of Unecessary Imports

--- PresentationLayer/nicaraguaApp.js
+++ PresentationLayer/nicaraguaApp.js
@@ -1,27 +1,12 @@
-var fs = require('fs');
 var http = require('http');
-var https = require('https');
-var privateKey = fs.readFileSync('./PresentationLayer/Local.key', 'utf8');
-var certificate = fs.readFileSync('./PresentationLayer/Local.crt', 'utf8');
-var credentials = { key: privateKey, cert: certificate };
-var WeatherService = require('../DomainLayer/Services/weatherService');
-var RegionService = require('../DomainLayer/Services/regionService');
 const express = require('express');
-var cors = require('cors');
 var app = express();
 const bodyparser = require('body-parser');
-const PermissionMiddleWare = require('./permissionMiddleWare');
-const JwtIssuer = require('./jwtIssuer');
 
 var httpServer = http.createServer(app);
-var httpsServer = https.createServer(credentials, app);
-const httpsPort = 3000;
-// httpServer.listen(81, () => console.log('Express server is running at port #80 HTTP'));
-httpsServer.listen(httpsPort, () => console.log(`Express server is running at port ${httpsPort} HTTPS`));
+const httpPort = 3000;
+httpServer.listen(httpPort, () => console.log(`Express server is running at port ${httpPort} HTTPS`));
 
-
-
-app.use(cors());
 app.use(bodyparser.json({ limit: '50mb' }))
 app.use(bodyparser.urlencoded({ extended: false }));
 
