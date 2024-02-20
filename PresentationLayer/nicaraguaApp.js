26f52fddc6a8f1d9348d36088190f3801e6c98f9
Giulliano Siviero <gsilvazanottisiviero@scu.edu>
2024-02-20 13:33:21 -0300

    Changes to make first deployment in Firebase Cloud Functions

--- PresentationLayer/nicaraguaApp.js
+++ PresentationLayer/nicaraguaApp.js
@@ -1,18 +1,7 @@
 var http = require('http');
-const { program } = require('commander');
-var cors = require('cors');
 const express = require('express');
-var corsOptions = {	origin: 'https://app.nicaagua.net' }
-
-program.requiredOption('-p, --port <type>', 'Port where the server will run.');
-program.parse(process.argv);
 var app = express();
-app.use(cors(corsOptions)); 
 const bodyparser = require('body-parser');
-
-var httpServer = http.createServer(app);
-const httpPort = program.opts().port;
-httpServer.listen(httpPort, () => console.log(`Express server is running at port ${httpPort} HTTPS`));
 
 app.use(bodyparser.json({ limit: '50mb' }))
 app.use(bodyparser.urlencoded({ extended: false }));
@@ -27,3 +16,5 @@ require('./Controllers/weatherController')(app);
 require('./Controllers/waterAlertController')(app);
 require('./Controllers/forecastController')(app);
 require('./Controllers/longTermForecastController')(app);
+
+module.exports =app;
